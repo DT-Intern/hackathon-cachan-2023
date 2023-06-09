80ff1a2493fc54293c958b22363369ea8e52360f
Maximilian Flügel <fluegel@hypros.de>
2023-06-10 00:28:01 +0200

    chore(ML):

--- controllers/controller_supervisor/controller_supervisor.py
+++ controllers/controller_supervisor/controller_supervisor.py
@@ -1,17 +1,39 @@
 from controller import Supervisor
+from time import sleep
 
 supervisor = Supervisor()
 receiver = supervisor.getDevice("receiver")
 receiver.enable(64)
+emitter = supervisor.getDevice("emitter")
 
 tt_02 = supervisor.getFromDef("TT02")
 tt_02_translation = tt_02.getField("translation")
 tt_02_rotation = tt_02.getField("rotation")
 
+initial_translation = tt_02_translation.getSFVec3f()
+initial_rotation = tt_02_rotation.getSFRotation()
+
+# Transmit all checkpoints
 while supervisor.step(int(supervisor.getBasicTimeStep())) != -1:
+    # Receive all incoming messages
     if receiver.getQueueLength() > 0:
         message = receiver.getData().decode("utf-8")
         if message == "reset":
-            tt_02_translation.setSFVec3f([-2, -2.5, 0.1])
-            tt_02_rotation.setSFRotation([0, 0, 1, 0])
+            tt_02_translation.setSFVec3f(initial_translation)
+            tt_02_rotation.setSFRotation(initial_rotation)
+            # tt_02_translation.setSFVec3f([-2, -2.5, 0.1])
+            # tt_02_rotation.setSFRotation([0, 0, 1, 0])
+        elif message == "checkpoints":
+            checkpoints_data = []
+            emitter.send(f"c{0}, {initial_translation[0]}, {initial_translation[1]}, {initial_translation[2]}".encode("utf-8"))
+            for i in range(10):
+                optional_checkpoint = supervisor.getFromDef(f"checkpoint{str(i)}")
+                if optional_checkpoint is not None:
+                    checkpoint_position = optional_checkpoint.getPosition()
+                    emitter.send(
+                        f"c{str(i + 1)}, {checkpoint_position[0]}, {checkpoint_position[1]}, {checkpoint_position[2]}".encode(
+                            "utf-8"))
         receiver.nextPacket()
+
+    position = tt_02.getPosition()
+    emitter.send(f"p{position[0]}, {position[1]}, {position[2]}".encode("utf-8"))
--- controllers/controller_trainer/controller_trainer.py
+++ controllers/controller_trainer/controller_trainer.py
@@ -1,12 +1,26 @@
+import math
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 from keras.models import Sequential, load_model
+from keras.optimizers.legacy import Adam
 from keras.layers import Dense, Dropout, BatchNormalization
 from vehicle import Driver
 from controller import Lidar
 import numpy as np
 import random
+
+time_progressed_standing = 0
+reward = 0
+distance_traveled = 0
+prev_x = 0
+prev_y = 0
+previous_checkpoint_reward = 0
+checkpoints = []
+current_checkpoint = 1
+update_target_network = 1000  # Update target network every 1000 steps
+replay_buffer = []
+batch_size = 32
 
 pre_trainer_model = load_model("../controller_pre_trainer/pre_trainer_model.h5")
 
@@ -17,8 +31,8 @@ model.add(Dense(32, activation="relu"))
 model.add(Dropout(0.1))
 model.add(BatchNormalization())
 model.add(Dense(2))
+model.compile(loss="mean_squared_error", optimizer=Adam(learning_rate=0.0001))
 model.set_weights(pre_trainer_model.get_weights())
-model.compile(loss="mean_squared_error", optimizer="adam")
 
 target_model = Sequential()
 target_model.add(Dense(64, input_shape=(200,), activation="relu"))
@@ -27,10 +41,14 @@ target_model.add(Dense(32, activation="relu"))
 target_model.add(Dropout(0.1))
 target_model.add(BatchNormalization())
 target_model.add(Dense(2))
+target_model.compile(loss="mean_squared_error", optimizer=Adam(learning_rate=0.00001))
 target_model.set_weights(model.get_weights())
 
 driver = Driver()
 emitter = driver.getDevice("emitter")
+receiver = driver.getDevice("receiver")
+receiver.enable(64)
+emitter.send("checkpoints".encode("utf-8"))
 
 basicTimeStep = int(driver.getBasicTimeStep())
 sensorTimeStep = 4 * basicTimeStep
@@ -39,59 +57,97 @@ lidar = Lidar("lidar")
 lidar.enable(sensorTimeStep)
 lidar.enablePointCloud()
 
-distance_traveled = 0
-
 def collision_occurred(lidar_data, threshold_distance):
-    for distance in lidar_data:
-        if distance < threshold_distance:
-            return True
-    return False
+    return min(lidar_data) < threshold_distance
 
 
-def calculate_reward(speed, lidar_data):
+def calculate_reward(speed, lidar_data, checkpoint_reward):
     # Define reward weights
-    speed_weight = 1.2
-    collision_penalty = -20.0
-
-    # Check the distance to the wall
-    distance_diff = lidar_data[0] - lidar_data[-1]
-    distance_weight = 0.4
+    # speed_weight = 0.2
+    checkpoint_weight = 1.5
+    collision_penalty = -100.0
 
     # Check for collision
-    if collision_occurred(lidar_data, 0.2):
+    if collision_occurred(lidar_data, 0.15):
         return collision_penalty
 
+    # Calculate checkpoint reward
+    reward = checkpoint_reward * checkpoint_weight
+
+    # Calculate speed reward
+    # reward += speed * speed_weight
+
     # Calculate the overall reward
-    return (speed_weight * speed) - distance_diff * distance_weight
+    return reward
+    # Define reward weights
+    # speed_weight = 0.4
+    # checkpoint_weight = 0.8
+    # travel_weight = 0.5
+    # collision_penalty = -1000.0
+    # reward = 0
 
+    # Check for collision
+    # if collision_occurred(lidar_data, 0.2):
+        # return collision_penalty
 
-def set_speed_m_s(speed_m_s):
-    speed = speed_m_s * 3.6
-    if speed > 28:
-        speed = 28
-    if speed < 0:
-        speed = 0
-    driver.setCruisingSpeed(speed)
-    return speed
+    # if lidar_data[100] < 0.5:
+        # reward -= 50
 
-def set_direction_degree(angle_degree):
-    if angle_degree > 16:
-        angle_degree = 16
-    elif angle_degree < -16:
-        angle_degree = -16
-    angle = angle_degree * 3.14 / 180
-    driver.setSteeringAngle(angle)
-    return angle
+    # reward += (len(passed_checkpoints) + checkpoint_reward) * checkpoint_weight
+
+    # reward += distance_traveled * travel_weight
+
+    # Calculate the overall reward
+    # return reward + (speed_weight * speed)
+
 
 def epsilon_greedy_action(q_values, epsilon):
     if random.random() < epsilon:
-        return random.uniform(-1, 1), random.uniform(-1, 1)
+        return q_values[0] + random.uniform(-epsilon, epsilon), q_values[1] + random.uniform(-epsilon, epsilon)
     else:
         return q_values
 
-epsilon = 1.0  # Initial exploration rate
+
+def euclidian_distance(vector_a, vector_b):
+    return math.sqrt(math.pow(vector_b[0] - vector_a[0], 2) + math.pow(vector_b[1] - vector_a[1], 2))
+
+
+def update_checkpoints(position):
+    global current_checkpoint
+    # total_distance = 0
+    if len(checkpoints) < 2:
+        return 0
+
+    pos_x, pos_y, pos_z = map(float, position.split(", "))
+
+    id, old_x, old_y, old_z = map(float, checkpoints[current_checkpoint - 1].split(","))
+    id, x, y, z = map(float, checkpoints[current_checkpoint].split(", "))
+    checkpoint_distance = euclidian_distance([x, y], [pos_x, pos_y])
+    total_distance = euclidian_distance([old_x, old_y], [pos_x, pos_y])
+
+    if checkpoint_distance < 0.1:
+        current_checkpoint = current_checkpoint + 1
+
+    return (current_checkpoint - 1) + (total_distance - checkpoint_distance) / total_distance
+
+    # for i in range(len(available_checkpoints)):
+        # id, x, y, z = map(float, available_checkpoints[i].split(", "))
+        # checkpoint_distance = euclidian_distance([x, y], [pos_x, pos_y])
+        # total_distance += checkpoint_distance
+
+        # Add the checkpoint to the closed list
+        #if checkpoint_distance < 0.17:
+            # if id not in passed_checkpoints:
+                # passed_checkpoints.append(id)
+                # available_checkpoints.remove(available_checkpoints[i])
+
+    # return total_distance
+
+
+epsilon = 0.2  # Initial exploration rate
 epsilon_min = 0.01  # Minimum exploration rate
-epsilon_decay = 0.995  # Exploration rate decay factor
+epsilon_decay = 0.8  # Exploration rate decay factor
+
 
 print("Initialization complete")
 
@@ -99,41 +155,43 @@ while driver.step() == -1:
     pass
 
 print("Starting learning process")
-time_progressed_standing = 0
-update_target_network = 1000  # Update target network every 1000 steps
-replay_buffer = []
-batch_size = 32
 
 for episode in range(1000):
     print("Running simulation for epoch", episode)
     while distance_traveled < 1000:
-        if time_progressed_standing > 5000:
-            break
+        checkpoint_reward = 0
+        if receiver.getQueueLength() > 0:
+            message = receiver.getData().decode("utf-8")
+            if "c" in message:
+                checkpoints.append(message.replace("c", ""))
+            elif "p" in message:
+                checkpoint_reward = update_checkpoints(message.replace("p", ""))
+                # pos_x, pos_y, pos_z = map(float, message.replace("p", "").split(", "))
+                # distance_traveled += euclidian_distance([pos_x, pos_y], [prev_x, prev_y])
+                # prev_x = pos_x
+                # prev_y = pos_y
+            receiver.nextPacket()
+
         driver.step()
         lidar_data = lidar.getRangeImage()
         processed_data = np.array(lidar_data).reshape(1, 200)
 
         action = model.predict(processed_data)[0]
-        speed, steering = epsilon_greedy_action(action, epsilon)
+        # speed, steering = epsilon_greedy_action(action, epsilon)
+        speed, steering = action
 
-        if speed < 0:
+        if abs(speed) < 0.5:
             time_progressed_standing += basicTimeStep
 
         if np.isnan(speed) or np.isnan(steering):
             continue
 
-        if collision_occurred(lidar_data, 0.17):
-            break
-
-        if steering > 0.314:
-            steering = 0.3
-        elif steering < -0.314:
-            steering = -0.3
-
         driver.setCruisingSpeed(float(speed))
         driver.setSteeringAngle(float(steering))
 
-        reward = calculate_reward(speed, lidar_data)
+        reward += calculate_reward(speed, lidar_data, checkpoint_reward)
+        print(reward)
+        # print(distance_traveled)
         replay_buffer.append((processed_data, action, reward))
 
         if len(replay_buffer) >= batch_size:
@@ -141,28 +199,49 @@ for episode in range(1000):
             batch_data = [replay_buffer[i] for i in batch_indices]
             x_batch = np.concatenate([data[0] for data in batch_data])
             y_batch = np.array([data[1] for data in batch_data])
-            reward_batch = np.array([data[2] for data in batch_data])
 
-            q_values = model.predict(x_batch)
-            target_q_values = target_model.predict(x_batch)
+            model.fit(x_batch, y_batch, epochs=1, verbose=0)
 
-            for i, (_, action, reward) in enumerate(batch_data):
-                if not np.isnan(reward):
-                    q_values[i] = action + reward
-                else:
-                    q_values[i] = action
+        # if len(replay_buffer) >= batch_size:
+            # batch_indices = np.random.choice(len(replay_buffer), size=batch_size, replace=False)
+            # batch_data = [replay_buffer[i] for i in batch_indices]
+            # x_batch = np.concatenate([data[0] for data in batch_data])
+            # y_batch = np.array([data[1] for data in batch_data])
+            # reward_batch = np.array([data[2] for data in batch_data])
 
-            model.fit(x_batch, q_values, epochs=5, verbose=0)
+            # q_values = model.predict(x_batch)
+            # target_q_values = target_model.predict(x_batch)
+
+            # for i, (_, action, reward) in enumerate(batch_data):
+                # if not np.isnan(reward):
+                    # q_values[i] = action + reward
+                # else:
+                    # q_values[i] = action
+
+            # model.fit(x_batch, y_batch, epochs=1, verbose=0)
+
+        if collision_occurred(lidar_data, 0.25) or time_progressed_standing > 4000:
+            driver.setCruisingSpeed(0)
+            driver.setSteeringAngle(0)
+            break
 
         if driver.getTime() % update_target_network == 0:
             target_model.set_weights(model.get_weights())
 
-        distance_traveled += speed * basicTimeStep / 1000
-        print(distance_traveled)
         epsilon = max(epsilon * epsilon_decay, epsilon_min)
+        previous_checkpoint_reward = checkpoint_reward
 
-    emitter.send("reset".encode("utf-8"))
-    distance_traveled = 0
     time_progressed_standing = 0
+    distance_traveled = 0
+    prev_x = 0
+    prev_y = 0
+    reward = 0
+    # previous_checkpoint_reward =
+    current_checkpoint = 1
+    #for i in range(len(passed_checkpoints)):
+        #checkpoint = passed_checkpoints[i]
+        #available_checkpoints.append(checkpoint)
+        #passed_checkpoints.remove(checkpoint)
+    emitter.send("reset".encode("utf-8"))
 
 model.save("car_model.h5")
